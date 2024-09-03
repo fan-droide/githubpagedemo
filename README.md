61cb3dd25cfde21c67bc54a7590af79a65d8df89
fan-droide <jose.gilpanal@telecom-paris.fr>
2024-09-03 15:09:39 +0200

    test new repo

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # githubpagedemo
+
+https://www.matteogregoricchio.com/articles/github-pages-hosting-with-parcel
+
+https://hackmd.io/@HucTd2OFR8OrulXPUZynzA/r15ggol72
+
+https://gist.github.com/poonia/7f6e688738b808d448adc0d4e8bef65a
+
+
+https://gist.github.com/OleksiyRudenko/7e94aa2d18927e0c5e65b2b5a1c0e375
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,17 @@
-console.log('hello')
+import { TestLatencyMLS} from './test.js'
+
+const debugCanvas = true
+
+const TEST_LAT_MLS_BTN_ID = 'testlatencymlsbtn'
+
+const constraints = { audio: {echoCancellation:false, noiseSuppression:false, autoGainControl:false, latency: 0, channelCount: 1 }}
+
+const main = async () => {
+    
+    const stream = await navigator.mediaDevices.getUserMedia(constraints)
+
+    const ac = new AudioContext({latencyHint:0})
+
+    TestLatencyMLS.initialize(ac, stream, TEST_LAT_MLS_BTN_ID, debugCanvas)
+}
+main()
